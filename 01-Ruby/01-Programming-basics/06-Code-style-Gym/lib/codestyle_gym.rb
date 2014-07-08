47a1e38716a98976c618bfddd9f86409a823f4f2
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-08 17:05:53 +0200

    triche (pas de modifs)

--- 01-Ruby/01-Programming-basics/06-Code-style-Gym/lib/codestyle_gym.rb
+++ 01-Ruby/01-Programming-basics/06-Code-style-Gym/lib/codestyle_gym.rb
@@ -1,17 +1,17 @@
 class Player_moving
   Grid_POSITIONS = (1..100).to_a
-  
-def initialize ( position ) 
-  @position = position ; @iterations = 0; 
+
+def initialize ( position )
+  @position = position ; @iterations = 0;
 raise "Initial position must be between 1 and 100" unless Grid_POSITIONS.include? @position
   end
-  
+
       def move   distanceInMeters
 @position += distanceInMeters
   end
-  
+
 def play
-  if(  self.status_Player==:play   ) 
+  if(  self.status_Player==:play   )
 if rand(1..100) < 50 ; move(rand(1..10))
   else
         move(-rand(1..10))
@@ -20,17 +20,17 @@ end
 end
 
 def cheat; @position = 101; end
-  
+
 def status_Player
     if( @position >   100 )
-      :win 
+      :win
     elsif(@position<0) ; :loose
     else;  :play
     end
   end
-  
+
   def hasWon ; status_Player  == :win ; end
-    
+
 end
 
 player = Player_moving.new(50)
@@ -38,9 +38,11 @@ player.play
 puts player.status_Player == :play # true
 player.cheat
 puts player.hasWon == true
-  
+
 begin
   Player_moving.new(110)
 rescue RuntimeError => e
   puts e.to_s == "Initial position must be between 1 and 100" # true
 end
+
+# test test
