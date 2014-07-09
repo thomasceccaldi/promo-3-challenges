fa672c0f34a81f7eae9196fd5f6815d187ec9c63
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-09 16:23:59 +0200

    exercice 3 incomplet

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,11 +1,24 @@
+# TODO: Use Random to get a new random score
+
 def bank_score
-  # TODO: Use Random to get a new random score
+  bank_score =rand(16..21)
+
 end
+
+# TODO: Use Random to get a new random card
 
 def pick_card
-  # TODO: Use Random to get a new random card
+  pioche = rand(1..1)
+
 end
 
+# TODO: Take the bank and the score and output an array containing the bank and then the score
+
 def game_outcome(bank, score)
-  # TODO: Take the bank and the score and output an array containing the bank and then the score
+
+  game_table = []
+  game_table << bank_score
+  game_table << player_score
+  return game_table
+
 end
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -1,17 +1,48 @@
 require_relative 'black_jack'
 
+
+#TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+
 def play_game
-  #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+
+  puts "Card ? (type 'Y' or 'yes' for a new card)"
+  response = gets.chomp.to_s
+  while response == "yes" || response == "Y"
+    puts "Card ? (type 'Y' or 'yes' for a new card)"
+    response = gets.chomp.to_s
+  end
+
 end
+
+# TODO: Returns custom message with player's score and bank's score
 
 def state_of_the_game(score, bank)
-  # TODO: Returns custom message with player's score and bank's score
+  return "Your score is #{score}, bank is #{bank}!"
+
 end
+
+# TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
 
 def asking_for_card?(score)
-  # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+  if score <= 21
+    play_game
+    if response == "yes" || response == "Y"
+      return true
+  else
+    return false
+  end
 end
 
+#TODO: return specific message depending on the game outcome (= bank's score and user's score)
+
 def build_message_for(outcome)
-  #TODO: return specific message depending on the game outcome (= bank's score and user's score)
+
+  case outcome
+    when outcome(1) > 21
+    puts "You are over 21... you loose."
+    else
+      puts ""
+  end
+
+end
 end
