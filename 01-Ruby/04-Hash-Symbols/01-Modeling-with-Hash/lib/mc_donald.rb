added9e105d15ff836e6edfe8d98065fa3d05f6b
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-11 12:15:50 +0200

    premier essai exerice 04.01

--- 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
+++ 01-Ruby/04-Hash-Symbols/01-Modeling-with-Hash/lib/mc_donald.rb
@@ -1,7 +1,73 @@
+#TODO: return number of calories for this mcDonald order
+
 def poor_calories_counter(burger, side, beverage)
-  #TODO: return number of calories for this mcDonald order
+
+  burgers = {
+    "Cheese Burger" => 290,
+    "Big Mac" => 300,
+    "Mc Bacon" => 400
+  }
+
+  sides = {
+  "Royal Cheese" => 130,
+  "French fries" => 130,
+  "Potatoes" => 130
+  }
+
+  beverages = {
+  "Coca" => 160,
+  "Sprite" => 170
+  }
+
+  somme_cal = 0
+
+  burgers.each do |key, value|
+    if key == burger
+    somme_cal += value
+    end
+  end
+
+  sides.each do |key, value|
+    if key == side
+    somme_cal += value
+    end
+  end
+
+  beverages.each do |key, value|
+    if key == beverage
+    somme_cal += value
+    end
+  end
+
+return somme_cal
+
 end
 
+poor_calories_counter("Cheese Burger", "French fries", "Coca")
+
+
+#TODO: return number of calories for a less constrained order
+#
+# on va retourner dans la methode un tableau ["French fries", "Happy Meal", "Sprite"]
+#
+
+
+
 def calories_counter(*orders)
-  #TODO: return number of calories for a less constrained order
+
+
+  ordered_items = *orders
+
+  ordered_items.each do |key, value|
+    if key == orders
+    somme_cal += value
+    end
+  end
+
 end
+
+calories_counter("French fries", "Sprite")
+
+
+
+
