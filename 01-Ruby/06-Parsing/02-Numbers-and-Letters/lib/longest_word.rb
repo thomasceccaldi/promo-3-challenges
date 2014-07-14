c73f115279066dd0dc308a92baa5d06c4fd40b62
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-14 19:42:50 +0200

    exercice 6.2 presque ok

--- 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
+++ 01-Ruby/06-Parsing/02-Numbers-and-Letters/lib/longest_word.rb
@@ -4,11 +4,84 @@ require 'json'
 def generate_grid(grid_size)
   #TODO: generate random grid of letters
 
-p [*('A'..'Z')].sample(9).join
-
+  Array.new(grid_size) { ("A".."Z").to_a.sample }
 
 end
 
 def run_game(attempt, grid, start_time, end_time)
   #TODO: runs the game and return detailed hash of result
+
+  results = {}
+
+  results[:time] = end_time - start_time
+
+  if check_grid?(attempt, grid)
+
+    traduction = translate(attempt)
+
+    if traduction
+
+      if attempt.size < grid
+
+        results[:score] = attempt.size + end_time - start_time + 3
+        results[:message] = "well done"
+        results[:translation] = traduction
+      else
+        results[:score] = attempt.size + end_time - start_time + 10
+        results[:message] = "well done"
+        results[:translation] = traduction
+
+      end
+
+    else
+      results[:score] = 0
+      results[:message] = "not an english word"
+
+    end
+
+
+  else
+
+    results[:score] = 0
+    results[:message] = "not in the grid"
+
+  end
+
+  results
+
 end
+
+def translate(attempt)
+
+  api_url = "http://api.wordreference.com/0.8/80143/json/enfr/#{attempt}"
+
+  open(api_url) do |stream|
+    definition = JSON.parse(stream.read)
+
+    if definition.has_key?("Error")
+      traduction = nil
+
+    else
+      traduction = definition["term0"]["PrincipalTranslations"]["0"]["FirstTranslation"]["term"]
+    end
+    return traduction
+  end
+end
+
+
+def check_grid?(attempt, grid)
+
+  attempt_grid = attempt.split("")
+
+  attempt_grid.all? { |letter| attempt_grid.count(letter) <= grid.count(letter) }
+
+end
+
+
+
+
+
+
+
+
+
