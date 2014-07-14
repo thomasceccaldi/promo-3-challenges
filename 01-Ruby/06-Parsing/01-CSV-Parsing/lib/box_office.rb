1cf5026674ceb8e222c9ee8479a5b7d2a0658b77
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-14 15:40:28 +0200

    exercice 06.01 ok

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -3,28 +3,26 @@ require 'csv'
 require "date"
 
 
-
-
 def most_successfull(number, max_year, file_name=nil)
   #TODO: return the number most successfull movies max_year$
+
 
   csv_options = { col_sep: ',', headers: :first_row}
   filepath    = '/Users/thomasceccaldi/code/thomasceccaldi/promo-3-challenges/01-Ruby/06-Parsing/01-CSV-Parsing/lib/movies.csv'
 
+  successfull_movies = []
+
   CSV.foreach(filepath, csv_options) do |row|
 
-    successfull_movies = []
 
-    if row[year] < max_year
-      successfull_movies = [row["name"]]
-      return successfull_movies
+    if row["year"].to_i < max_year
 
-    else
-      return false
+      successfull_movies << {name: row[0], year: row[1].to_i, earnings: row[2].to_i}
     end
-
   end
 
-most_successfull(10, 1995)
+  successfull_movies.sort! { |row| row["earnings"].to_i }
+
+  successfull_movies.take(number)
 
 end
