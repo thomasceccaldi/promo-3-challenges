fa3aff8ae0f048fcb8b496055b79b6235d73eefe
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-15 15:15:13 +0200

    exercice 05.02 partiel

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,8 +1,21 @@
 def anagrams?(a_string, another_string)
   #TODO: implement the obvious method to test if two words are anagrams
+
+  #test : "hello".split(//).sort "ollhe".split(//).sort
+
+  return a_string.split(//).sort == another_string.split(//).sort
+
+
 end
+
+
+
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
+
+  # HASH
+
+
+
 end
-  
