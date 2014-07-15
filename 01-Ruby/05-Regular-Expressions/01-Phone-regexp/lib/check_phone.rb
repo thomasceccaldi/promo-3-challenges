5876e00e9eebd99e2d981bd46b18a767d7947972
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-15 11:18:40 +0200

    exercice 05.01 ok mais erreurs à corriger

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,7 +1,14 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
 
-   /(0|[+]33)[^ ]\d{8}/ !~ "phone_number"
+  if "phone_number"  =~  /(0|[+]33)6\d{7}/ >= 0
+
+    return true
+  else
+
+    return false
+  end
 
 
 end
+
