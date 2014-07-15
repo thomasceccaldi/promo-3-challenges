80a2ca26e99dfba44a53df387c9623be34f22d8f
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-15 17:04:24 +0200

    exercice 5.2

--- 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
+++ 01-Ruby/05-Regular-Expressions/02-Anagrams/lib/anagrams.rb
@@ -1,21 +1,43 @@
 def anagrams?(a_string, another_string)
   #TODO: implement the obvious method to test if two words are anagrams
-
-  #test : "hello".split(//).sort "ollhe".split(//).sort
-
-  return a_string.split(//).sort == another_string.split(//).sort
-
+  return a_string.downcase.scan(/\w/).sort == another_string.downcase.scan(/\w/).sort
 
 end
-
-
 
 
 def anagrams_on_steroids?(a_string, another_string)
   #TODO: implement the improved method
-
   # HASH
 
+  a_tab = a_string.downcase.chars
 
+  another_tab = another_string.downcase.chars
+
+  hash_a = {}
+
+    a_tab.each do |c|
+      if c=~ /\w/
+        if hash_a[c]
+          hash_a[c] += 1
+         else
+          hash_a[c] = 1
+        end
+      end
+    end
+
+  hash_another = {}
+
+      another_tab.each do |c|
+        if c=~ /\w/
+          if hash_another[c]
+            hash_another[c] += 1
+           else
+            hash_another[c] = 1
+          end
+        end
+      end
+
+    hash_another == hash_a
 
 end
+
