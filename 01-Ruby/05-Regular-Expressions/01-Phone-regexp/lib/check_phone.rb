421476222072577ea2fd96831f6bdc2dc0330f91
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-15 12:12:03 +0200

    exercice 05.01 non def

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,14 +1,24 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
 
-  if "phone_number"  =~  /(0|[+]33)6\d{7}/ >= 0
+clean_num = clean_phone_num(phone_number)
 
+  if /(0|[+]33)6\d{8}/.match(clean_num) >= 0
     return true
   else
-
     return false
   end
 
+end
+
+
+def clean_phone_num(phone_number)
+
+    return "phone_number".gsub(/\W/,"")
 
 end
+
+
+
+
 
