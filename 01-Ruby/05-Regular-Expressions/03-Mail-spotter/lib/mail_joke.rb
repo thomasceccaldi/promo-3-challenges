d22193349df4fe2598ef62d83972867e2fe859f9
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-15 18:03:15 +0200

    exerice 05.03

--- 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
+++ 01-Ruby/05-Regular-Expressions/03-Mail-spotter/lib/mail_joke.rb
@@ -2,4 +2,29 @@
 
 def mail_joke(email)
   # TODO: Return a joke suited to the email provided
+
+  domain_name = email.split("@").delete_at(0)
+
+  if email =~ /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+
+    case domain_name.to_s
+
+    when gmail.com
+      puts "sympa"
+
+    when hotmail.com
+      puts "naze"
+
+    when yahoo.com
+      puts "parents"
+
+    when aol.com
+      puts "old"
+
+    else
+      p "Well done boris, you're skilled and capable"
+    end
+
+  end
+
 end
