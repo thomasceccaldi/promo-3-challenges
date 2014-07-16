bbe0152a3f2d4eb7a63fea0821df9a02d9c1cda2
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-16 14:43:29 +0200

    exerice 2.1.3 non terminé

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,3 +1,37 @@
 class OrangeTree
   # TODO: Implement all the specs defined in the README.md :)
+
+  attr_accessor :height, :age, :fruits_produced, :dead
+
+  def initialize
+    @height = 0
+    @age = 0
+    @fruits = 0
+    @dead = false
+  end
+
+  def one_year_passes!
+
+    @age += 1
+    @fruits = 0
+
+    if @age <= 10
+      @height += 1
+    end
+
+    if age >= 50 && rand(101-@age) == 0
+        @dead = true
+    end
+  end
+
+
+  def pick_a_fruit!
+
+    @fruits -= 1
+  end
+
 end
+
+
+
+
