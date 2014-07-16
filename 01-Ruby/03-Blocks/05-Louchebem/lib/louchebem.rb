d262178f773eb1e0843678a59e6493451ac47dae
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-16 10:18:07 +0200

    louchebem pas fini

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -1,5 +1,28 @@
 # Encoding: utf-8
+#TODO: implement your louchebem translator
+
+ # diviser la phrase en mots
+ # remplacer la première lettre par un l
+
+
+  # ajouter à la fin du mot : ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+
+  # si un mot commencer par une voyelle "ajout d'un suffixe différent"
+
+  # ne pas remplacer les mots de un caractère
+
+  # === > fonction string.size
+
+  # si un mot commence par deux consonnes ou plus l'intégrer au debut
+
 
 def louchebemize(sentence)
-  #TODO: implement your louchebem translator
+
+  words = sentence.split
+
+
+  each.to_s.words[0] = l
+
 end
+
+louchebemize(hello blabla blabla)
