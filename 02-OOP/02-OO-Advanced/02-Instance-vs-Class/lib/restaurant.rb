32d97ed53160e1a132812c7f105a126466f42c16
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-17 11:46:15 +0200

    exerice 2.2.2 (reste une erreur)

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,32 @@
 class Restaurant
-  
+
   #TODO add relevant accessors if necessary
-  
+  attr_reader :city, :name
+  attr_accessor :average_rating, :rates
+
   def initialize(city, name)
     #TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @rates=[]
+    @average_rating = 0
+
   end
-  
+
+  def rate(grade)
+    @rates << grade
+    @average_rating = rates.sum.to_f / rates.size
+  end
+
   #TODO: implement #filter_by_city and #rate methods
-  
+
+  def Restaurant.filter_by_city(city_selected)
+
+    restaurants_selected = []
+
+    if @city == "city_selected"
+      restaurants_selected << @name
+    end
+  end
+
 end
