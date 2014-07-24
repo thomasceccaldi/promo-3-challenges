14226b60270149c45db77953fdf3140a74d64761
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-07-24 13:04:34 +0200

    exercice 3.1.3 ok

--- 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
+++ 03-AR-Database/01-DB-Design-and-SQL/03-Interacting-with-code/lib/queries.rb
@@ -6,16 +6,38 @@ db = SQLite3::Database.new(database_path)
 
 def number_of_rows(db, table_name)
   #TODO: count number of rows in table table_name
+
+array = db.execute( "SELECT COUNT(*) FROM #{table_name};" )
+
+array.first.first
+
 end
 
 def sorted_artists(db)
   #TODO: return array of artists' names sorted alphabetically
+
+array = db.execute( "SELECT Name FROM Artist ORDER BY name ASC;")
+
+array.map! { |x| x.first }
+
+
 end
 
 def love_tracks(db)
   #TODO: return array of love songs
+
+array = db.execute( "SELECT Name FROM Track WHERE Name LIKE '%love%';")
+array.map! { |x| x.first }
+
+
 end
 
 def long_tracks(db, min_length)
   #TODO: return tracks verifying: duration > min_length (minutes)
+
+  array = db.execute( "SELECT Name FROM Track WHERE Milliseconds > #{min_length*60000} ")
+  array.map! { |x| x.first }
+
 end
+
+
