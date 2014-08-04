8fdc6eb0c22f24fc3cb00819841711fd28b55aa1
Thomas Ceccaldi <thomasceccaldi@gmail.com>
2014-08-04 16:07:11 +0200

    exerice 1 JQuery ok

--- 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
+++ 04-Front-End/04-Javascript-jQuery/01-DOM-Manipulation/application.js
@@ -8,46 +8,54 @@
 function run_challenges() {
 
   // Ex 1. Read the content of the email input
-  var email = null;  // TODO: replace null, keep the email variable.
-
+  var email = $('#email').attr('value');  // TODO: replace null, keep the email variable.
 
   // Ex 2. Fill the content of the email input with your email
 
+  //$('#email').val("thomasceccaldi@gmail.com");
 
+  $('#email').attr('value','thomasceccaldi@gmail.com');
 
   // Ex 3. Replace the email hint (next to the input) with 'This is my email now'
   //       The text should be emphasized using a <strong> tag
 
+  $('#email-hint').html("<strong>This is my email now</strong>");
 
   // Ex 4. Add the .blue CSS class to the table header cells (th elements)
 
-
+  $('table > thead > tr').addClass('blue');
 
   // Ex 5. Count the number of table body rows there are (team count!)
   var team_count = 0;  // TODO: replace 0, keep the team_count variable.
 
-
+  team_count = $('table > tbody > tr').length;
 
   // Ex 6. Say there is a 15th team added to the table.
   //       Add a row at the bottom, this new team should have zero points.
 
 
+  $('table > tbody').append('<tr><td>15</td><td>Tassin la demi-lune</td><td>0</td></tr>');
+
 
   // Ex 7. Write some code to sum all points given to all teams
   var sum = 0;  // TODO: replace 0 with your sum result, keep the sum variable.
 
-
+  $('tbody tr').each(function(i, value) {
+      sum += parseInt($(value).find('td:last').text());
+  });
 
   // Ex 8. Change the background color of all table header cells to #DDF4FF
 
-
+  $('table > thead > tr > th').css("background-color", "#DDF4FF");
 
   // Ex 9. Translate the #results list 20px downward (animation would be great ^_^)
 
+  $('#results-wrapper').css('padding-top', '20px');
 
 
   // Ex 10. Remove the "Email:" label from the DOM
 
+  $('label').remove();
 
 
   // Checking exercise answers. DO NOT MODIFY BELOW
